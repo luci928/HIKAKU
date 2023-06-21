1f1ae9e57d3778d7635464392332739ae01e5993
ru0505 <127956898+ru0505@users.noreply.github.com>
2023-06-21 08:26:19 -0500

    Add files via upload

---
+++ fichita.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+
+class ficha {
+ private:
+      char letra;
+public:
+      ficha(char l) {
+          letra = l;
+  } char obtenerletra() {
+            return letra;
+   }
+};
+
---
+++ movi_01.cpp
@@ -0,0 +1,184 @@
+#include <ctime>
+#include <cstdlib>
+#include <stdlib.h>
+#include <iomanip>
+#include <iostream>
+#include "fichita.h"
+#include <time.h>
+
+
+using namespace std;
+const int fil = 5;
+const int col = 3;
+
+class MATRIZ {
+private:
+    int _Matriz[fil][col];
+    vector <ficha> fichas;
+public:
+    MATRIZ() {
+
+
+        for (int f = 0; f < fil; f++) {// futuramente cambiar a la original
+            for (int c = 0; c < col; c++) {
+                _Matriz[f][c] = 0;
+            }
+        }
+    }
+
+    void llenar() {
+
+        srand(time(0));
+        for (int f = 0; f < fil; f++) {
+            for (int c = 0; c < col; c++) {
+                _Matriz[f][c] = rand() % 9;
+            }
+        }
+    }
+
+    void mostrar() {
+        for (int f = 0; f < fil; f++) {
+            for (int c = 0; c < col; c++) {
+                cout << setw(3) << _Matriz[f][c] << " ";
+            }cout << endl;
+        }
+    }
+    void cambiar(int a) {
+
+        srand(time(NULL));
+        
+        /*i = 1 + rand() % (5 + 1 - 1);*/
+
+        for (int i=0;i< a; i++) {//recorrido
+            if (i == 1) {
+                char ga = _Matriz[i - 1][2];
+                _Matriz[i - 1][2] = 1;
+                char ga1 = _Matriz[i - 1][1];
+                char ga2 = _Matriz[i - 1][0];
+                _Matriz[i - 1][1] = ga;
+                _Matriz[i - 1][0] = ga1;
+
+            }if (i == 2) {
+                char ga = _Matriz[i - 1][2];
+                _Matriz[i - 1][2] = 2;
+                char ga1 = _Matriz[i - 1][1];
+                char ga2 = _Matriz[i - 1][0];
+                _Matriz[i - 1][1] = ga;
+                _Matriz[i - 1][0] = ga1;
+
+            }if (i == 3) {
+                char ga = _Matriz[i - 1][2];
+                _Matriz[i - 1][2] = 3;
+                char ga1 = _Matriz[i - 1][1];
+                char ga2 = _Matriz[i - 1][0];
+                _Matriz[i - 1][1] = ga;
+                _Matriz[i - 1][0] = ga1;
+
+            }if (i == 4) {
+                char ga = _Matriz[i - 1][2];
+                _Matriz[i - 1][2] = 4;
+                char ga1 = _Matriz[i - 1][1];
+                char ga2 = _Matriz[i - 1][0];
+                _Matriz[i - 1][1] = ga;
+                _Matriz[i - 1][0] = ga1;
+
+            }if (i == 5) {
+                char ga = _Matriz[i - 1][2];
+                _Matriz[i - 1][2] = 5;
+                char ga1 = _Matriz[i - 1][1];
+                char ga2 = _Matriz[i - 1][0];
+                _Matriz[i - 1][1] = ga;
+                _Matriz[i - 1][0] = ga1;
+
+            }
+            /*
+            char ga = _Matriz[2][i + 1];//guardor
+            _Matriz[2][i + 1] = a;//cambiar
+            char ga2 = _Matriz[a - i][i + 1];
+            _Matriz[2][i + 1] = ga;
+            _Matriz[2][i + 1] = ga2;*/
+
+
+            //valor
+            
+            }
+
+        }
+
+
+    void cambiar_01(int a) {
+
+        srand(time(NULL));
+
+        /*i = 1 + rand() % (5 + 1 - 1);*/
+
+        for (int i = 0; i < a; i++) {//recorrido
+            if (i == 1) {
+                char ga = _Matriz[i - 1][1];
+                _Matriz[i - 1][1] = 1;
+                char ga2 = _Matriz[i - 1][2];
+                char ga1 = _Matriz[i - 1][0];
+                _Matriz[i - 1][2] = ga1;
+                _Matriz[i - 1][0] = ga;
+                
+
+            }if (i == 2) {
+                char ga = _Matriz[i - 1][1];
+                _Matriz[i - 1][1] = 2;
+                char ga2 = _Matriz[i - 1][2];
+                char ga1 = _Matriz[i - 1][0];
+                _Matriz[i - 1][2] = ga1;
+                _Matriz[i - 1][0] = ga;
+
+            }if (i == 3) {
+                char ga = _Matriz[i - 1][1];
+                _Matriz[i - 1][1] = 3;
+                char ga2 = _Matriz[i - 1][2];
+                char ga1 = _Matriz[i - 1][0];
+                _Matriz[i - 1][2] = ga1;
+                _Matriz[i - 1][0] = ga;
+
+            }if (i == 4) {
+                char ga = _Matriz[i - 1][1];
+                _Matriz[i - 1][1] = 4;
+                char ga2 = _Matriz[i - 1][2];
+                char ga1 = _Matriz[i - 1][0];
+                _Matriz[i - 1][2] = ga1;
+                _Matriz[i - 1][0] = ga;
+
+            }if (i == 5) {
+                char ga = _Matriz[i - 1][1];
+                _Matriz[i - 1][1] = 5;
+                char ga2 = _Matriz[i - 1][2];
+                char ga1 = _Matriz[i - 1][0];
+                _Matriz[i - 1][2] = ga1;
+                _Matriz[i - 1][0] = ga;
+
+            }
+            /*
+            char ga = _Matriz[2][i + 1];//guardor
+            _Matriz[2][i + 1] = a;//cambiar
+            char ga2 = _Matriz[a - i][i + 1];
+            _Matriz[2][i + 1] = ga;
+            _Matriz[2][i + 1] = ga2;*/
+
+
+            //valor
+
+        }
+
+    }
+    };
+
+    int main() {
+        MATRIZ Ma1;
+        Ma1.llenar();
+        Ma1.mostrar();
+        cout << endl;
+        Ma1.cambiar(6);
+        Ma1.mostrar();
+        cout << endl;
+        Ma1.cambiar_01(6);
+        Ma1.mostrar();
+        return 0;
+    }
