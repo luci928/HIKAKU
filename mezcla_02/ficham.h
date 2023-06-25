d6de53cb2a55510f62c97be09c806ff8287d719c
ru0505 <127956898+ru0505@users.noreply.github.com>
2023-06-24 20:03:45 -0500

    Add files via upload

---
+++ mezcla_02/ficham.h
@@ -0,0 +1,18 @@
+#include <ctime>
+#include <cstdlib>
+#include <stdlib.h>
+#include <iomanip>
+#include <iostream>
+#include "fichita.h"
+#include <time.h>
+#ifndef  FICHAM_H
+#define FICHAM_H
+
+
+void ficham() {
+    
+    int x;
+    x = 1 + rand() % (5 - 1);
+    return ;
+}
+#endif
